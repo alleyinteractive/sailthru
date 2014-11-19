5fad10d17917d124261d296ed8d20b303d65fc31
Nicole Arnold <nicole@alleyinteractive.com>
2014-11-18 20:34:14 -0500

    whitespace/syntax cleanup

--- classes/class-sailthru-scout.php
+++ classes/class-sailthru-scout.php
@@ -1,10 +1,7 @@
 <?php
 /*
- *
- *	Scout depends entirely on Horizon
- *
+ *	Scout depends entirely on Horizon.
  */
-
 class Sailthru_Scout_Widget extends WP_Widget {
 
 	/*--------------------------------------------*
@@ -12,16 +9,13 @@ class Sailthru_Scout_Widget extends WP_Widget {
 	 *--------------------------------------------*/
 	function __construct() {
 
-		// Register Scout Javascripts
+		// Register Scout Javascripts.
 		add_action( 'wp_enqueue_scripts', array( $this, 'register_scout_scripts' ) );
 
-		// Attempt to create the page needed for Scout
+		// Attempt to create the page needed for Scout.
 		$post_id = $this->create_scout_page();
 
-		// Attempt to register the sidebar widget for Scout
-		//wp_register_sidebar_widget('sailthru-recommend-widget', 'Sailthru Recommends', array($this, 'widget'));
-
-		// load plugin text domain
+		// Load plugin text domain.
 		add_action( 'init', array( $this, 'load_widget_text_domain' ) );
 
 
@@ -29,8 +23,8 @@ class Sailthru_Scout_Widget extends WP_Widget {
 			'sailthru-recommends-id',
 			__( 'Sailthru Recommends Widget', 'sailthru-for-wordpress' ),
 			array(
-				'classname'		=>	'Sailthru_Scout',
-				'description'	=>	__( 'Sailthru Scout but in a compact sidebar widget.', 'sailthru-for-wordpress' )
+				'classname'	  => 'Sailthru_Scout',
+				'description' => __( 'Sailthru Scout but in a compact sidebar widget.', 'sailthru-for-wordpress' )
 			)
 		);
 
@@ -41,155 +35,136 @@ class Sailthru_Scout_Widget extends WP_Widget {
 	/* Public Functions
 	/*--------------------------------------------------*/
 
-	/**
-	 * Loads the Widget's text domain for localization and translation.
-	 */
+	// Loads the Widget's text domain for localization and translation.
 	public function load_widget_text_domain() {
 
 		load_plugin_textdomain( 'sailthru-for-wordpress', false, plugin_dir_path( SAILTHRU_PLUGIN_PATH ) . '/lang/' );
 
-	} // end load_widget_text_domain
+	} // End load_widget_text_domain.
 
 
 
-	/**
-	 * Add scout. But only if scout is turned on.
-	 */
+	// Add scout. But only if scout is turned on.
 	public function register_scout_scripts() {
 
-		$params = get_option('sailthru_scout_options');
+		$params = get_option( 'sailthru_scout_options' );
 
-		// is scout turned on?
-		if( isset($params['sailthru_scout_is_on']) &&  $params['sailthru_scout_is_on']) {
+		// Is scout turned on?
+		if ( isset( $params['sailthru_scout_is_on'] ) &&  $params['sailthru_scout_is_on'] ) {
 
-			// Check first, otherwise js could throw errors
-			if( get_option('sailthru_setup_complete') ) {
+			// Check first, otherwise js could throw errors.
+			if ( get_option( 'sailthru_setup_complete' ) ) {
 
-				//wp_enqueue_script( 'sailthru-scout', '//ak.sail-horizon.com/scout/v1.js', array('jquery', 'sailthru-horizon') );
-
-				//wp_enqueue_script( 'sailthru-scout-params', SAILTHRU_PLUGIN_URL .'/js/scout.params.js' , array('sailthru-scout') );
-
-				// if conceirge is on, we want noPageView to be set to true
-				// see
-				$conceirge = get_option('sailthru_concierge_options');
-					if( isset($conceirge['sailthru_convierge_is_on'] ) && $conceirge['sailthru_convierge_is_on'] ) {
+				// If conceirge is on, we want noPageView to be set to true
+				$conceirge = get_option( 'sailthru_concierge_options' );
+					if( isset( $conceirge['sailthru_convierge_is_on'] ) && $conceirge['sailthru_convierge_is_on'] ) {
 						$params['sailthru_scout_noPageview'] = 'true';
 					}
 
-				add_action('wp_footer', array( $this, 'scout_js' ), 10);
+				add_action( 'wp_footer', array( $this, 'scout_js' ), 10 );
 
-				//wp_localize_script( 'sailthru-scout-params', 'Scout', $params );
+			} // End if sailthru setup is done.
 
-			} // end if sailthru setup is done
+		} // End if scout is on.
 
-		} // end if scout is on
-
-	} // register_conceirge_scripts
+	} // End register_conceirge_scripts.
 
 	/*-------------------------------------------
 	 * Utility Functions
 	 *------------------------------------------*/
 
 	/**
-	 * A function used to render the Scout JS f
+	 * A function used to render the Scout JS.
 	 *
-	 * @returns  string
+	 * @return string
 	 */
 	 function scout_js() {
 
-	 	$options = get_option('sailthru_setup_options');
+	 	$options        = get_option( 'sailthru_setup_options' );
 		$horizon_domain = $options['sailthru_horizon_domain'];
-		$scout = get_option('sailthru_scout_options');
-		$scout_params = array();
+		$scout          = get_option( 'sailthru_scout_options' );
+		$scout_params   = array();
 
 		// inlcudeConsumed?
-		if( isset($scout['sailthru_scout_includeConsumed']) ) {
-			$scout_params[] = strlen( $scout['sailthru_scout_includeConsumed'] ) > 0 ?  'includeConsumed: '. (bool) $scout['sailthru_scout_includeConsumed'].'' : '';
+		if ( isset( $scout['sailthru_scout_includeConsumed'] ) ) {
+			$scout_params[] = strlen( $scout['sailthru_scout_includeConsumed'] ) > 0 ?  'includeConsumed: ' . (bool) $scout['sailthru_scout_includeConsumed'] . '' : '';
 		} else {
 			$scout['sailthru_scout_includeConsumed'] = '';
 		}
 
 		// renderItem?
-		if( isset( $scout['sailthru_scout_renderItem']) ) {
-			$scout_params[] = strlen($scout['sailthru_scout_renderItem']) > 0 ?  "renderItem: ". (bool) $scout['sailthru_scout_renderItem']."": '';
+		if( isset( $scout['sailthru_scout_renderItem'] ) ) {
+			$scout_params[] = strlen( $scout['sailthru_scout_renderItem'] ) > 0 ?  "renderItem: " . (bool) $scout['sailthru_scout_renderItem'] . "": '';
 		} else {
 			$scout['sailthru_scout_renderItem'] = '';
 		}
 
-		if( isset( $scout['scout_num_visible']) ) {
-			$scout_params[] = strlen($scout['scout_num_visible']) > 0 ?  "numVisible:'". esc_js( $scout['sailthru_scout_number'] )."' ": '';
+		if( isset( $scout['scout_num_visible'] ) ) {
+			$scout_params[] = strlen( $scout['scout_num_visible'] ) > 0 ?  "numVisible:'" . esc_js( $scout['sailthru_scout_number'] ) . "' " : '';
 		} else {
 			$scout['scout_num_visible'] = '';
 		}
 
-
-		if ($scout['sailthru_scout_is_on'] == 1) {
+		if ( $scout['sailthru_scout_is_on'] == 1 ) {
 			echo "<script type=\"text/javascript\" src=\"//ak.sail-horizon.com/scout/v1.js\"></script>";
 		 	echo "<script type=\"text/javascript\">\n";
-	           echo "SailthruScout.setup({\n";
-	           echo "domain: '". esc_js($options['sailthru_horizon_domain'])."',\n";
-				if( is_array($scout_params) ) {
-					foreach ($scout_params as $key => $val) {
-						if (strlen($val) >0)  {
-							echo esc_js($val).",\n";
-						}
+	        echo "SailthruScout.setup({\n";
+	        echo "domain: '". esc_js( $options['sailthru_horizon_domain'] )."',\n";
+			if( is_array( $scout_params ) ) {
+				foreach ( $scout_params as $key => $val ) {
+					if ( strlen( $val ) > 0 )  {
+						echo esc_js( $val ) . ",\n";
 					}
 				}
-	           echo "});\n";
-
-		     echo " if(SailthruScout.allContent.length == 0) { jQuery('#sailthru-scout').hide() }";
-		     echo "</script>\n";
+			}
+	        echo "});\n";
+		    echo " if(SailthruScout.allContent.length == 0) { jQuery('#sailthru-scout').hide() }";
+		    echo "</script>\n";
 		}
 
 	 }
 
 	/**
-	 * A function used to programmatically create a page needed for Scout. The slug, author ID, and title
-	 * are defined within the context of the function.
+	 * A function used to programmatically create a page needed for Scout.
+	 * The slug, author ID, and title are defined within the context of the function.
 	 *
-	 * @returns -1 if the post was never created, -2 if a post with the same title exists, or the ID
-	 *          of the post if successful.
+	 * @return -1 if the post was never created, -2 if a post with the same title exists,
+	 * or the ID of the post if successful.
 	 */
-
 	private function create_scout_page() {
 
-		// never run this on public facing pages
-		if( !is_admin() ) {
+		// Never run this on public facing pages.
+		if ( ! is_admin() ) {
 			return;
 		}
 
 		// -1 = No action has been taken.
 		$post_id = -1;
 
-		// Our specific settings
-		$slug = 'scout-from-sailthru';
-		$title = 'Recommended for You';
-		$post_type = 'page';
+		// Our specific settings.
+		$slug         = 'scout-from-sailthru';
+		$title        = 'Recommended for You';
+		$post_type    = 'page';
 		$post_content = '<div id="sailthru-scout"><div class="loading">Loading, please wait...</div></div>';
 
-
-		// If the page doesn't already exist, then create it
+		// If the page doesn't already exist, then create it.
 		$create_page = function_exists( 'wpcom_vip_get_page_by_title' ) ? null == wpcom_vip_get_page_by_title( $title ) : null == get_page_by_title( $title );
-		if( $create_page ) {
-
-			// Set the post ID so that we know the post was created successfully
+		if ( $create_page ) {
+			// Set the post ID so that we know the post was created successfully.
 			$post_id = wp_insert_post(
 				array(
-					'comment_status'	=>	'closed',
-					'ping_status'		=>	'closed',
-					'post_name'			=>	$slug,
-					'post_title'		=>	$title,
-					'post_status'		=>	'publish',
-					'post_type'			=>	$post_type,
-					'post_content'		=>	$post_content
+					'comment_status' => 'closed',
+					'ping_status'    => 'closed',
+					'post_name'      => $slug,
+					'post_title'     => $title,
+					'post_status'    => 'publish',
+					'post_type'	     => $post_type,
+					'post_content'   => $post_content,
 				)
 			);
-
 		} else {
-
 	    	$post_id = -2;
-
-		} // end if
+		} // End if.
 
 		return $post_id;
 
@@ -200,42 +175,39 @@ class Sailthru_Scout_Widget extends WP_Widget {
 	/* Widget API Functions
 	/*--------------------------------------------------*/
 
-
 	/**
 	 * Outputs the content of the widget.
 	 *
-	 * @param	array	args		The array of form elements
-	 * @param	array	instance	The current instance of the widget
+	 * @param array args     The array of form elements.
+	 * @param array instance The current instance of the widget.
 	 */
-	function widget($args, $instance) {
+	function widget( $args, $instance ) {
 
 		extract( $args, EXTR_SKIP );
-
 		echo $before_widget;
-
 		include( SAILTHRU_PLUGIN_PATH . 'views/widget.scout.display.php' );
-
 		echo $after_widget;
+
 	}
 	/**
 	 * Processes the widget's options to be saved.
 	 *
-	 * @param	array	new_instance	The previous instance of values before the update.
-	 * @param	array	old_instance	The new instance of values to be generated via the update.
+	 * @param array new_instance The previous instance of values before the update.
+	 * @param array old_instance The new instance of values to be generated via the update.
 	 */
 	public function update( $new_instance, $old_instance ) {
 
 		$instance = array();
-			$instance['title'] = filter_var( $new_instance['title'], FILTER_SANITIZE_STRING );
+		$instance['title'] = filter_var( $new_instance['title'], FILTER_SANITIZE_STRING );
 
 		return $instance;
 
-	} // end widget
+	} // End widget.
 
 	/**
 	 * Generates the administration form for the widget.
 	 *
-	 * @param	array	instance	The array of keys and values for the widget.
+	 * @param array instance The array of keys and values for the widget.
 	 */
 	public function form( $instance ) {
 
@@ -245,16 +217,13 @@ class Sailthru_Scout_Widget extends WP_Widget {
                 'title' => ''
             )
         );
-        $title = esc_attr($instance['title']);
+        $title = esc_attr( $instance['title'] );
 
 
-		// Display the admin form
+		// Display the admin form.
 		include( SAILTHRU_PLUGIN_PATH . 'views/widget.scout.admin.php' );
 
-	} // end form
+	} // End form.
+} // End class.
 
-
-
-
-} // end class
-add_action( 'widgets_init', create_function( '', 'register_widget("Sailthru_Scout_Widget");' ) );
+add_action( 'widgets_init', create_function( '', 'register_widget( "Sailthru_Scout_Widget" );' ) );
--- views/widget.scout.display.php
+++ views/widget.scout.display.php
@@ -1,28 +1,16 @@
 <?php
-
-    /*
-     * If Scout is not on, advise the user
-     */
+    // If Scout is not on, advise the user.
     $scout = get_option( 'sailthru_scout_options' );
-
-    if( ! isset( $scout['sailthru_scout_is_on'] ) ||  ! $scout['sailthru_scout_is_on'] ) {
-
-        // do nothing, get outta here
+    if ( ! isset( $scout['sailthru_scout_is_on'] ) ||  ! $scout['sailthru_scout_is_on'] ) {
+        // Do nothing, get outta here.
         return;
-
     }
-
-    /*
-     * Grab the settings from $instance and fill out default
-     * values as needed.
-     */
+    // Grab the settings from $instance and fill out default values as needed.
 	$title = empty( $instance['title'] ) ? ' ' : apply_filters( 'widget_title', $instance['title'] );
-
 ?>
  <div class="sailthru-recommends-widget">
-
     <?php
-        // title
+        // Title.
         if ( ! empty( $title ) ) {
             if ( ! isset( $before_title ) ) {
                 $before_title = '';
@@ -33,7 +21,5 @@
             echo $before_title . trim( $title ) . $after_title;
         }
     ?>
-
 	<div id="sailthru-scout"><div class="loading">Loading, please wait...</div></div>
-
 </div>
